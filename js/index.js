7a89640cff707e8601624b3312871208a03b8305
Vanderlugt, Blair <blair.vanderlugt@gmail.com>
2016-09-23 16:58:14 -0700

    new

--- js/index.js
+++ js/index.js
@@ -1,53 +1,44 @@
-'use-strict';
+'use-strict'
 
+const globalVars = {}
 
-var Clock = function(runTime) {
-  console.log('clock instance created');
-  this.runTime = runTime;
-  this.breakTime = false;
-  this.running = false;
-  // this.endTime = endTime;
-  this.currentTime = Date.parse(new Date());
-  this.targetTime = (runTime*60*1000);
-  this.timeRemaining = new Date(this.currentTime + this.targetTime);
-  console.log(this.timeRemaining);
+var Clock = function (runTime) {
+  console.log('clock instance created')
+  this.runTime = runTime
+  this.breakTime = false
+  this.running = false
+  // this.endTime = this.endTime
+  let durationOfTimerInMilis = (runTime * 60 * 1000)
+  let endTime = new Date(Date.parse(new Date()) + durationOfTimerInMilis)
+  this.timeRemaining = Date.parse(endTime) - Date.parse(new Date())
 
   // reduce timeReamining by one second
-  this.decrementSecond = function() {
-    this.timeRemaining -= 1000;
+  this.decrementSecond = function () {
+    this.timeRemaining -= 1000
   }
 
   // returns time left in miliseconds
-  this.getTimeRemaining = function() {
-    return Date.parse(this.timeRemaining) - Date.parse(new Date());
+  this.getTimeRemaining = function () {
+    return this.timeRemaining
   }
 
-  this.startBreak = function() {
-    this.breakTime = true;
+  this.toggleBreak = function () {
+    this.breakTime = !this.breakTime
   }
 
-  this.endBreak = function() {
-    this.breakTime = false;
+  this.toggleRunning = function () {
+    this.running = !this.running
   }
-
-  this.startRunning = function() {
-    this.running = true;
-  }
-
-  this.stopRunning = function() {
-    this.running = false;
-  }
-
 }
 
-function convertMilisToSecondMinutes(milis) {
+function convertMilisToSecondMinutes (milis) {
   // var t = Date.parse(endtime) - Date.parse(new Date());
-  console.log("im gonna parse some shit " + milis);
-  var seconds = Math.floor( (milis/1000) % 60);
-  var minutes = Math.floor( (milis/1000/60) % 60);
+  // console.log('im gonna parse some shit ' + milis)
+  var seconds = Math.floor((milis / 1000) % 60)
+  var minutes = Math.floor((milis / 1000 / 60) % 60)
   return {
-    "minutes": minutes,
-    "seconds": seconds
+    'minutes': minutes,
+    'seconds': seconds
   }
 }
 
@@ -87,61 +78,68 @@ function convertMilisToSecondMinutes(milis) {
 //   return timeinterval;
 // }
 
-
-function updateTimer(id, type) {
+function updateTimer (id, type) {
   // take dom id and interval or break and return the new
   // number after button click
-  var target = document.getElementById(id);
-  var m = target.innerHTML;
-  if (type === "minus") {
+  var target = document.getElementById(id)
+  var m = target.innerHTML
+  if (type === 'minus') {
     if (parseInt(m) > 1) {
-      var newM = parseInt(m) - 1;
+      var newM = parseInt(m) - 1
     } else {
-      var newM = parseInt(m);
+      newM = parseInt(m)
     };
   } else {
-    var newM = parseInt(m) + 1;
+    newM = parseInt(m) + 1
   };
-  return newM;
+  return newM
 }
 
 // set a global var to determine clock state
 // running means the timer is in progress
 // var running = false;
-/// isBreak
+// / isBreak
 // var isBreak = false;
 
-$(document).ready(function() {
-  var newInterval = document.getElementById("interval").innerHTML;
-  var clock = document.getElementById("timer")
-  clock.querySelector(".minutes").innerHTML = ("0" + newInterval).slice(-2);
-  clock.querySelector(".seconds").innerHTML = "00"
-  var runTime = document.getElementById("interval").innerHTML;
-  pomodoroClock = new Clock(runTime);
+$(document).ready(function () {
+  var newInterval = document.getElementById('interval').innerHTML
+  var clock = document.getElementById('timer')
+  clock.querySelector('.minutes').innerHTML = ('0' + newInterval).slice(-2)
+  clock.querySelector('.seconds').innerHTML = '00'
+  var runTime = document.getElementById('interval').innerHTML
+  console.log("runTime is " + runTime)
+  globalVars.pomodoroClock = new Clock(runTime)
 })
 
-$(".circle").on("click", function() {
+var refreshIntervalId
 
-  if (pomodoroClock.running) {
-    console.log("and I'm back in here now " + pomodoroClock.running)
-    clearInterval(refreshIntervalId);
-    pomodoroClock.stopRunning();
+$('.circle').on('click', function () {
+  console.log("running state of clock is " + globalVars.pomodoroClock.running)
+
+  // when running returns true
+  if (globalVars.pomodoroClock.running) {
+    console.log(globalVars.clearInterval)
+    clearInterval(refreshIntervalId)
+    // globalVars.pomodoroClock.toggleRunning()
   }
 
-  console.log("timer clicked and clock status is: " + pomodoroClock.running);
-  if (!pomodoroClock.running) {
-    console.log("can I get in here? " + pomodoroClock.running);
-    pomodoroClock.startRunning();
-    console.log("can I has running? " + pomodoroClock.running);
+  // when running returns false
+  if (!globalVars.pomodoroClock.running) {
+    // console.log('can I get in here? ' + globalVars.pomodoroClock.running)
+    // globalVars.pomodoroClock.toggleRunning()
+    // console.log('can I has running? ' + globalVars.pomodoroClock.running)
 
-    var refreshIntervalId = setInterval(function() {
-      pomodoroClock.decrementSecond();
+    refreshIntervalId = setInterval(function () {
+      globalVars.pomodoroClock.decrementSecond()
       // update view
-      var minutesLeft = convertMilisToSecondMinutes(pomodoroClock.getTimeRemaining()).minutes
-      var secondsLeft = convertMilisToSecondMinutes(pomodoroClock.getTimeRemaining()).seconds
-      document.getElementById("timer").querySelector(".minutes").innerHTML = minutesLeft;
-      document.getElementById("timer").querySelector(".seconds").innerHTML = secondsLeft;
-    }, 1000);
+      var minutesLeft = convertMilisToSecondMinutes(globalVars.pomodoroClock.getTimeRemaining()).minutes
+      var secondsLeft = convertMilisToSecondMinutes(globalVars.pomodoroClock.getTimeRemaining()).seconds
+      document.getElementById('timer').querySelector('.minutes').innerHTML = ('0' + minutesLeft).slice(-2)
+      document.getElementById('timer').querySelector('.seconds').innerHTML = ('0' + secondsLeft).slice(-2)
+    }, 1000)
+
+    // toggle the state of running
+    globalVars.pomodoroClock.toggleRunning()
   }
 
   //   var clock = document.getElementById("timer");
@@ -176,40 +174,40 @@ $(".circle").on("click", function() {
 // controllers for the duration of timer and break
 // adjustments are allowed at anytime, but adjusting a running clock pauses the clock
 
-$("#minusInterval").on("click", function() {
+$('#minusInterval').on('click', function () {
   // if (!running) {
-    running = false;
-    var newInterval = updateTimer("interval", "minus");
-    document.getElementById("interval").innerHTML = newInterval;
-    var clock = document.getElementById("timer")
-    clock.querySelector(".minutes").innerHTML = ("0" +   newInterval).slice(-2);
-    clock.querySelector(".seconds").innerHTML = "00";
+    // running = false;
+  var newInterval = updateTimer('interval', 'minus')
+  document.getElementById('interval').innerHTML = newInterval
+  var clock = document.getElementById('timer')
+  clock.querySelector('.minutes').innerHTML = ('0' + newInterval).slice(-2)
+  clock.querySelector('.seconds').innerHTML = '00'
   // }
 })
 
-$("#addInterval").on("click", function() {
+$('#addInterval').on('click', function () {
   // if (!running) {
-    running = false;
-    var newInterval = updateTimer("interval", "add");
-    document.getElementById("interval").innerHTML = newInterval;
-    var clock = document.getElementById("timer")
-    clock.querySelector(".minutes").innerHTML = ("0" + newInterval).slice(-2);
-    clock.querySelector(".seconds").innerHTML = "00";
+    // running = false;
+  var newInterval = updateTimer('interval', 'add')
+  document.getElementById('interval').innerHTML = newInterval
+  var clock = document.getElementById('timer')
+  clock.querySelector('.minutes').innerHTML = ('0' + newInterval).slice(-2)
+  clock.querySelector('.seconds').innerHTML = '00'
   // }
 })
 
-$("#minusBreak").on("click", function() {
+$('#minusBreak').on('click', function () {
   // if (!running) {
-    running = false;
-    var newInterval = updateTimer("break", "minus");
-    document.getElementById("break").innerHTML = newInterval;
+    // running = false;
+  var newInterval = updateTimer('break', 'minus')
+  document.getElementById('break').innerHTML = newInterval
   // }
 })
 
-$("#addBreak").on("click", function() {
+$('#addBreak').on('click', function () {
   // if (!running) {
-    running = false;
-    var newInterval = updateTimer("break", "add");
-    document.getElementById("break").innerHTML = newInterval;
+    // running = false;
+  var newInterval = updateTimer('break', 'add')
+  document.getElementById('break').innerHTML = newInterval
   // }
 })
